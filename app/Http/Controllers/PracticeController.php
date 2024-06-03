a0d621272fa30804d47ae381364da7ef9cc0efa2
zulqarnain <maniacrypto97@gmail.com>
2024-06-03 15:25:03 +0500

    new change

--- app/Http/Controllers/PracticeController.php
+++ app/Http/Controllers/PracticeController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PracticeController extends Controller
 {
+    public function index(Request $request){
+       
+        $user=User::find(1);
+        dd($user);
+    }
     //
 }
