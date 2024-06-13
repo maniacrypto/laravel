4ea892452d7446bd4efd5d54b57b7f01bb25e00c
zulqarnain haider <maniacrypto97@gmail.com>
2024-06-14 03:43:43 +0500

    NPM

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 });
 
 Route::get('index',[PracticeController::class,'index']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
