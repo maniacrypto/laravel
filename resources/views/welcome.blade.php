0764e92846e0cb910c044b48b7df9173689e04c6
zulqarnain haider <maniacrypto97@gmail.com>
2024-06-17 01:59:46 +0500

    Testing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        @vite(['resources/js/app.js'])
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -20,7 +21,7 @@
             }
         </style>
     </head>
-    <body class="antialiased" id="app">
+    <body class="antialiased"  id="app">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
